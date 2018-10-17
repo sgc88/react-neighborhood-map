34cfe698f8436b360521cf796325100d7397c34b
sgc88 <sehribancheng@yahoo.com>
2018-10-16 23:16:10 -0700

    added locations

---
+++ src/location/locations.js
@@ -0,0 +1,73 @@
+export const locations = [
+  {
+    name: 'Ice Cream Museum',
+    location: {
+      address: '1 Grant Ave, San Francisco, CA 94108',
+      lat: 37.7870,
+      lng: -122.4052,
+
+    }
+  },
+
+  {
+    name: 'Palace of Fine rts',
+    location: {
+      address: '3301 Lyon St, San Francisco, CA 94123',
+      lat: 37.8030,
+      lng: -122.4492,
+
+    }
+  },
+
+  {
+    name: 'Alcatraz Heliport',
+    location: {
+      address: 'San Francisco, CA 94133',
+      lat: 37.8270,
+      lng: -122.4230,
+
+    }
+  },
+
+  {
+    name: 'Golden Gate Bridge',
+    location: {
+      address: 'Golden Gate Bridge, San Francisco, CA',
+      lat: 37.8199,
+      lng: -122.4783,
+
+    }
+  },
+
+  {
+    name: 'Cable Car Barn and Museum',
+    location: {
+      address: '1201 Mason St, San Francisco, CA 94108',
+      lat: 37.4741,
+      lng: -122.2442,
+
+    }
+  },
+
+  {
+    name: 'Cliff House',
+    location: {
+      address: '1090 Point Lobos Ave, San Francisco, CA 94121',
+      lat: 37.7785,
+      lng: -122.5140,
+
+    }
+  },
+
+  {
+    name: 'DeYoung Museum',
+    location: {
+      address: '50 Hagiwara Tea Garden Dr, San Francisco, CA 94118',
+      lat: 37.4617,
+      lng: -1222.809,
+
+    }
+  },
+
+
+  ]
